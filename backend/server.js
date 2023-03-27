dd5276efae2967b5b27d3e6b88c5eae5c8bdb313
MARY SANNI <marysanni@MARYs-MacBook-Pro.local>
2023-03-27 10:22:37 -0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js.js'
+import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
